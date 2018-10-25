f27611c453c4502d3aee14fed3056ad3e01c2d66
Habenzy <robertrstauss@gmail.com>
2018-10-25 16:41:16 -0400

    restaurant page now populates sidebar with info

--- main.js
+++ main.js
@@ -23,17 +23,18 @@ async function getRestarauntPages() {
     })
 };
 
-function populateRestaurantPage(resty) {
-  let allRestaraunts = []
-  let sidebar = document.getElementById('sidebar');  
-  console.log(sidebar.innerHTML)
-  fetch('http://localhost:8080/all.json')
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (ids) {
-      ids.forEach(id => {
-        allRestaraunts.push(id + '.json')
-      });
-    })
+function populateRestaurantPage() {
+  let path = window.location.href;
+  let restaurantId = path.split('#')[1];
+  let currentRestaurant = restaurantId + '.json';
+  let sidebar = document.getElementById('sidebar');
+  fetch(`http://localhost:8080/${currentRestaurant}`)
+  .then((response) => {
+    return response.json()
+  })
+  .then((thisJson) => {
+    sidebar.innerHTML += `<br><div class="sidebar-title"><b>${thisJson.name}<b></div><br><br>${thisJson.phone}<br><br>${thisJson.address}<br><br>${thisJson.email}<br><br>${thisJson.notes}`;
+    return thisJson
+  })
+  
 }
