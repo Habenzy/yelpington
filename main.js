c4d6226068961d42a469e997c09613dbb37ac923
Habenzy <robertrstauss@gmail.com>
2018-10-25 15:41:52 -0400

    basic restaurant page layout, very little functionality

--- main.js
+++ main.js
@@ -18,11 +18,23 @@ async function getRestarauntPages() {
             return response.json()
           })
           .then((restaraunt) => {
-            console.log(restaraunt);
-            sidebar.innerHTML += `<p class="restaraunt" id="${restaraunt.id}"><a href="index.html#${restaraunt.name.split(' ').join('+')}">${restaraunt.name}</a></p>`
-            console.log(sidebar);
-            console.log(restaraunt.name);
+            sidebar.innerHTML += `<p class="restaraunt" id="${restaraunt.id}"><a href="restaurant.html#${restaraunt.id}">${restaraunt.name}</a></p>`
           })
       })
     })
 };
+
+function populateRestaurantPage(resty) {
+  let allRestaraunts = []
+  let sidebar = document.getElementById('sidebar');  
+  console.log(sidebar.innerHTML)
+  fetch('http://localhost:8080/all.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (ids) {
+      ids.forEach(id => {
+        allRestaraunts.push(id + '.json')
+      });
+    })
+}
