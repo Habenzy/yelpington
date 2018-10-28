307c0f71753c0c3cc1937db3df93973bda4e4e4f
jbpayne <jonathan.b.payne@gmail.com>
2018-10-28 11:09:19 -0400

    rewrote the test to check for formatted content

--- cypress/integration/yelpington.spec.js
+++ cypress/integration/yelpington.spec.js
@@ -7,10 +7,14 @@ describe('Cypress', function () {
   });
   it('shows the info for a restaurant', function () {
     cy.visit('/restaurant.html#mr-mikes');
-    cy.get('body').contains('Mr. Mike\'s')
-    cy.get('body').contains('The greatest pizza around!')
-    cy.get('body').contains('864-0072')
-    cy.get('body').contains('206 Main St.')
+    cy.get('.sidebar-title').contains('Mr. Mike\'s')
+    cy.get('#sidebar ul li')
+    .should(($li) => {
+      expect($li).to.have.length(3)
+      expect($li.first()).to.contain('The greatest pizza around!')
+      expect($li.next()).to.contain('864-0072')
+      expect($li.last()).to.contain('206 Main St.')
+    });
   });
 });
 
