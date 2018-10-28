150e327bb16ea85dc60990ae4da3ed82a40fa28a
jbpayne <jonathan.b.payne@gmail.com>
2018-10-28 12:09:36 -0400

    added a restaurant page map test and main page restaurant link test

--- cypress/integration/yelpington.spec.js
+++ cypress/integration/yelpington.spec.js
@@ -19,5 +19,19 @@ describe('Cypress', function () {
       expect($li.last()).to.contain('206 Main St.')
     });
   });
+  it('shows a map centered at the restaurant\'s location', function () {
+    cy.visit('/restaurant.html#mr-mikes');
+    cy.get('div.leaflet-proxy.leaflet-zoom-animated')
+    .should('have.css', 'transform', 'matrix(131072, 0, 0, 131072, 1.99069e+07, 2.42782e+07)')
+  });
+  it('lists links to all restaurants on the main page', function () {
+    cy.visit('/index.html');
+    cy.get('.restaraunt')
+    .should(($restaurant) => {
+      expect($restaurant).to.have.length(16)
+    });
+    cy.get('#mr-mikes a')
+    .should('have', 'restaurant.html#mr-mikes')
+  });
 });
 
