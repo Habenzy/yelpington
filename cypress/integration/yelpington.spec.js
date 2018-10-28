06fa012731bb6e0bd5dd3634b7eda812aecd50b0
jbpayne <jonathan.b.payne@gmail.com>
2018-10-28 11:28:13 -0400

    added font-family and color to test

--- cypress/integration/yelpington.spec.js
+++ cypress/integration/yelpington.spec.js
@@ -5,9 +5,12 @@ describe('Cypress', function () {
   it('successfully visits the home page', function () {
     cy.visit('/');
   });
-  it('shows the info for a restaurant', function () {
+  it('shows the info for a restaurant formatted and styled', function () {
     cy.visit('/restaurant.html#mr-mikes');
     cy.get('.sidebar-title').contains('Mr. Mike\'s')
+    .should('have.css', 'font-family', 'Questrial, serif')
+    .should('have.css', 'color', 'rgb(27, 90, 50)')
+
     cy.get('#sidebar ul li')
     .should(($li) => {
       expect($li).to.have.length(3)
